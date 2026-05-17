cc5e71dbdc1be1a6a62c4c4a83e5f0c593009be4
Abdallah Ahmed <abdallahahmed.0523017@gmail.com>
2026-05-17 18:24:08 +0300

    feat: initialize React client project with Vite and setup server directory

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to MindWaves Client</h1>
+      <p>This is a React app built with Vite.</p>
+    </>
+  )
+}
+
+export default App
