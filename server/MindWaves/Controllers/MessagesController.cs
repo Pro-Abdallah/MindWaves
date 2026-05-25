ad8ccb087d3b5bcc51660325afd87b30d8b0c046
Nour <nourelden.0523066@gmail.com>
2026-05-26 02:26:59 +0300

    feat: Implement Create Message endpoint V1

--- server/MindWaves/Controllers/MessagesController.cs
+++ server/MindWaves/Controllers/MessagesController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MindWaves.Data;
+using MindWaves.Data.Models;
+using MindWaves.DTOs;
 
 namespace MindWaves.Controllers
 {
@@ -26,6 +28,22 @@ namespace MindWaves.Controllers
                 return NotFound("There aren't any Messages Right Now");
 
             return Ok(messages);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage(MessageDto dto)
+        {
+            var message = new Message
+            {
+                Content = dto.Content,
+                CreatedAt = DateTime.Now
+            };
+
+            _context.Add(message);
+            await _context.SaveChangesAsync();
+
+            return Ok("Message Created Successfully");
         }
     }
 }
