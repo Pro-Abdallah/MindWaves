b9d336f515227aa1f46eefdd90861480011895f9
Nour <nourelden.0523066@gmail.com>
2026-05-26 01:57:46 +0300

    chore: add base Messages Controller setup

---
+++ server/MindWaves/Controllers/MessagesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MindWaves.Data;
+
+namespace MindWaves.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        public MindWavesDbContext _context;
+
+        public MessagesController(MindWavesDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+    }
+}
